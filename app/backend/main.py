e2125cf2cbdb64d8978ce7018ee4b6ae40a6d5d6
CASA <chamyang0024@gmail.com>
2026-04-28 19:05:13 +0900

    Update main.py

--- app/backend/main.py
+++ app/backend/main.py
@@ -20,7 +20,7 @@ Base.metadata.create_all(bind=engine)
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://.*\.vercel\.app|http://localhost:5173|http://127\.0\.0\.1:5173", Frontend URLs
+    allow_origin_regex=r"https://.*\.vercel\.app|http://localhost:5173|http://127\.0\.0\.1:5173",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
